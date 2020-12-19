f4a62c8fc1fc7e94ac11a36661dccdda797162b3
alb12-la <alb12@ucla.edu>
2020-12-19 12:55:19 -0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import * as gifshot from 'gifshot';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  private base64Gif: string;
+  running = false;
+  ngOnInit() {
+
+  }
+
+  createGif(arr: Array<any>) {
+    console.log('arr', arr);
+    gifshot.createGIF(
+      {
+        images: arr,
+        interval: .1,
+        gifHeight: 280,
+        gifWidth: 500
+
+      }, (obj) => {
+        if (obj.error) {
+          console.log('error', obj.error);
+          return;
+        }
+
+        this.base64Gif = obj.image;
+        console.log(obj);
+      });
+  }
+
+  onTextchanges(newString: string) {
+    this.running = true;
+    this.base64Gif = null;
+    console.log('printing', newString);
+    this.runApp(newString);
+  }
+
+  runApp(input: string) {
+    const arr = this.wordsToImageArray(input);
+    this.createGif(arr);
+
+  }
+
+  // Input a string, output an array of letters
+  wordsToImageArray(input: string) {
+    const chars = input.split('');
+    console.log(chars);
+    const returnArray = [];
+    chars.forEach(char => {
+      returnArray.push('assets/base-frames/step-1.jpg');
+      returnArray.push(this.lettersToPath2(char));
+      returnArray.push(this.lettersToPath(char));
+    });
+    console.log(returnArray);
+    return [...returnArray];
+  }
+
+
+
+  lettersToPath(letter: string) {
+    console.log('converting', letter);
+    if (letter === ' ') {
+      return `assets/step-3-letters/space.jpg`;
+    }
+
+    if (letter === '!') {
+      return `assets/step-3-letters/exclamation.jpg`;
+    }
+
+    return `assets/step-3-letters/${letter}.jpg`;
+  }
+  lettersToPath2(letter: string) {
+
+    if (letter === ' ') {
+      return `assets/step-2-letters/space.jpg`;
+    }
+
+    if (letter === '!') {
+      return `assets/step-2-letters/exclamation.jpg`;
+    }
+
+    return `assets/step-2-letters/${letter}.jpg`;
+  }
+
+
+}
